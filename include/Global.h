2e11738359069b5ef8746feb54252bd196f5e780
rscardua <roger@notoriun.com.br>
2024-10-18 10:49:41 -0300

    criação de variaveis globais

--- include/Global.h
+++ include/Global.h
@@ -13,4 +13,28 @@ extern SemaphoreHandle_t xDistanceMutex;
 // Variável global para armazenar a distância medida
 extern volatile int globalDistance;
 
+// Fator de divisão para ajustar a leitura da distância
+extern volatile int fator_divisao;
+
+// Início e fim da Zona 1
+extern volatile int zona1_inicio;
+extern volatile int zona1_fim;
+
+// Início e fim da Zona 2
+extern volatile int zona2_inicio;
+extern volatile int zona2_fim;
+
+// Início e fim da Zona 3
+extern volatile int zona3_inicio;
+extern volatile int zona3_fim;
+// Token de autenticação do MQTT
+extern std::string tokenMqt;
+
+// variavel com ip do servidor
+extern std::string ipServidor;
+
+// variavel com porta do servidor
+extern std::string portaServidor;
+
+
 #endif // GLOBALS_H
--- src/Global.cpp
+++ src/Global.cpp
@@ -1,5 +1,6 @@
 #include "Global.h"
 #include <cstddef>
+#include <string>
 
 // Definição das variáveis globais
 
@@ -8,3 +9,29 @@ SemaphoreHandle_t xDistanceMutex = NULL;
 
 // Variável global para armazenar a distância medida
 volatile int globalDistance = 0;
+
+// Fator de divisão para ajustar a medida da distância
+volatile int fator_divisao = 1;
+
+// Início e fim da Zona 1
+volatile int zona1_inicio = 0;
+volatile int zona1_fim = 0;
+
+// Início e fim da Zona 2
+volatile int zona2_inicio = 0;
+volatile int zona2_fim = 0;
+
+// Início e fim da Zona 3
+volatile int zona3_inicio = 0;
+volatile int zona3_fim = 0;
+
+// Token de autenticação do MQTT
+std::string tokenMqt = "";
+
+// variavel com ip do servidor
+std::string ipServidor = "";
+
+// variavel com porta do servidor
+std::string portaServidor = "";
+
+
